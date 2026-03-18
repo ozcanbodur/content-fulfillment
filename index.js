4f0fc5693c3dfe890d98c58cdae85fe6a71651c5
Ozcan Bodur <104697381+ozcanbodur@users.noreply.github.com>
2026-03-18 21:25:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { chromium } = require("playwright");
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => res.send("OK"));
 
